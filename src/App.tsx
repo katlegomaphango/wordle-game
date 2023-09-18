7e906774e98cda36371c6723ac8ba337fc881725
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-18 16:35:29 +0200

    styled comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Letter } from './components'
+import { AccuracyEnum } from './utilities/accuracy.utils'
 
 function App() {
 
   return (
     <>
-      <div>App</div>
+      <Letter accuracy={AccuracyEnum.correct} position={0} value='H' />
     </>
   )
 }
---
+++ src/components/Letter/index.style.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+
+export const StyledLetterButton = styled.button `
+    margin: 2px;
+    width: 50px;
+    height: 50px;
+    border-radius: 2px;
+    color: white;
+    font-size: 30px;
+    font-weight: bold;
+    background: green;
+`
