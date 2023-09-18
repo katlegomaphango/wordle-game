946d72f83f051364d6cc01a8546e117a87c3b13b
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-18 16:20:01 +0200

    btn

--- src/components/Letter/Letter.tsx
+++ src/components/Letter/Letter.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { AccuracyEnum } from "../../utilities/accuracy.utils";
 
-const Letter = () => {
-  return (
-    <div>Letter</div>
-  )
+interface ILetterProps {
+    position: number,
+    value: string,
+    accuracy: AccuracyEnum
+}
+
+const Letter = (props: ILetterProps) => {
+    const { position, value, accuracy } = props
+
+    return (
+        <>
+            <button style={{ width: '50px', height: '50px' }}>
+                {value}
+            </button>
+        </>
+    )
 }
 
 export default Letter;
