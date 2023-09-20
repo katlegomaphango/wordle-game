7bef267cd567b3189fca24016dcbe088566303f3
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-20 23:12:22 +0200

    word entry

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 import Letter from "./Letter/Letter";
 import Word from "./Word/Word";
+import WordEntry from "./WordEntry/WordEntry";
 
 export {
     Letter,
-    Word
+    Word,
+    WordEntry
 }
