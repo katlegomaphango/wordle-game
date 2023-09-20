7edf1dfc40d965a414057d618933befdaacad830
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-20 19:57:10 +0200

    accuracy

--- src/components/Word/Word.tsx
+++ src/components/Word/Word.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react"
 import { Letter } from ".."
 import { AccuracyEnum } from "../../utilities/accuracy.utils"
+import { evaluatedWordScore } from "../../utilities/evaluation.utils"
+
+
 
 interface IWordProps {
     isWordEvaluated: boolean,
@@ -10,28 +13,45 @@ interface IWordProps {
 const Word = (props: IWordProps) => {
     const { isWordEvaluated, guessWordValue } = props
 
+    const inititalAccuracyArray =[
+        AccuracyEnum.none,
+        AccuracyEnum.none,
+        AccuracyEnum.none,
+        AccuracyEnum.none,
+        AccuracyEnum.none
+    ]
+
     const [guessValue, setGuessValue] = useState('')
     const [isEvaluated, setIsEvaluated] = useState(false)
-
-    useEffect(() => {
-        setGuessValue(guessWordValue)
-    }, [guessWordValue])
+    const [evaluatedResults, setEvaluatedResults] = useState<AccuracyEnum[]>(inititalAccuracyArray)
 
     useEffect(() => {
         setIsEvaluated(isWordEvaluated)
     }, [isWordEvaluated])
+
+
+    //original
+    useEffect(() => {
+        //const results = evaluatedWordScore( guessValue, retrieveAnswer().toUpperCase() )
+        const results = evaluatedWordScore( guessValue, "REACT" )
+
+        setEvaluatedResults(results)
+        setGuessValue(guessWordValue)
+    }, [guessWordValue])
+
+    
 
     return (
         <>
             {
                 guessValue
                 .toUpperCase()
-                .split('-')
+                .split('')
                 .map( (nextLetter, index) => (
                     <Letter 
                         key={"letter_" + index} 
                         value={nextLetter}
-                        accuracy={AccuracyEnum.none}
+                        accuracy={isEvaluated ? evaluatedResults[index] : AccuracyEnum.none }
                         position={index}
                     />
                 ))
