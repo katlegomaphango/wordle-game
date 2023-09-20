c47c7299c589f1817f9b0cbfa19f3082e5410a49
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-20 23:13:04 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import './App.css'
-import { Letter, Word } from './components'
-import { AccuracyEnum } from './utilities/accuracy.utils'
+import { Word, WordEntry } from './components'
+//import { AccuracyEnum } from './utilities/accuracy.utils'
 
 function App() {
+  const [wordGuess, setWordGuess] = useState('')
+
+  const handleGuessCompletion = (guess: string): void => {
+
+  } 
 
   return (
     <>
-      <Word isWordEvaluated={false} guessWordValue='TESTS' />
+      <div>
+        <WordEntry
+          onGuessEntered={(guess) => setWordGuess(guess)} 
+          onGuessComplete={() => handleGuessCompletion(wordGuess)}
+        />
+        <Word guessWordValue={wordGuess} isWordEvaluated={false} />
+      </div>
     </>
   )
 }
